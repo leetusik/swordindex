ce20346b28d68d5548c34788c3b21b770fdb187d
leetusik <leetusik@gmail.com>
2025-03-05 14:57:49 +0900

    Asdf

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Add this after STATIC_URL setting
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
